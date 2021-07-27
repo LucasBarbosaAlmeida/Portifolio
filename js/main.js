4f0296adf712328f75a2254e8b9ca24537324c42
lucasba123 <lucas.barbosa.almeida9@gmail.com>
2021-07-27 20:16:43 -0300

    Versão 1.3

--- js/main.js
+++ js/main.js
@@ -10,14 +10,45 @@
         if(event.target.classList.contains("tab-item") && 
         !event.target.classList.contains("active")){
             const target = event.target.getAttribute("data-target");
-
             tabsContainer.querySelector(".active").classList.remove("outer-shadow","active");
-
             event.target.classList.add("active","outer-shadow");
-
             aboutSection.querySelector(".tab-content.active").classList.remove("active");
-            
             aboutSection.querySelector(target).classList.add("active");
+        }
+    })
+})();
+
+/* Portfolio filter */
+(() =>{
+
+    const filterContainer = document.querySelector(".portfolio-filter"),
+    portfolioItemsContainer = document.querySelector(".portfolio-items"),
+    portfolioItems = document.querySelectorAll(".portfolio-item"),
+    popup = document.querySelector(".portfolio-popup"),
+    prevBtn = popup.querySelector(".pp-prev"),
+    nextBtn = popup.querySelector(".pp-next"),
+    closeBtn = popup.querySelector(".pp-close"),
+    projectDetailsContainer = popup.querySelector(".pp-details"),
+    projectDetailsBtn = popup.querySelector(".pp-project-details-btn");
+    let itemIndex, slideIndex, screenshots;
+
+    filterContainer.addEventListener("click", (event)=>{
+        if(event.target.classList.contains("filter-item") &&
+        !event.target.classList.contains("active")){
+        /* deactivate existing active 'filter-item' */
+        filterContainer.querySelector(".active").classList.remove("outer-shadow","active");
+        event.target.classList.add("active","outer-shadow");
+        const target = event.target.getAttribute("data-target");
+        portfolioItems.forEach((item) =>{
+            if(target === item.getAttribute("data-category") || target === 'all'){
+                item.classList.remove("hide");
+                item.classList.add("show");
+            }
+            else{
+                item.classList.remove("show");
+                item.classList.add("hide");
+            }
+        })
         }
     })
 })();
